9fc36469bcd6f6c35e23130c18bd84e561b2ff37
Ola Idczak <olaa.idczak@gmail.com>
2026-03-16 19:06:26 +0100

    set up repo structure

---
+++ src/fista/lr.py
@@ -0,0 +1,18 @@
+class LogisticRegression():
+    def __init__(self):
+        pass
+
+    def fit(self, X_train, y_train):
+        pass
+
+    def validate(self, X_valid, y_valid, measure):
+        pass
+
+    def predict_proba(self, X_test):
+        pass
+
+    def plot(self, measure):
+        pass
+
+    def plot_coefficients(self):
+        pass
