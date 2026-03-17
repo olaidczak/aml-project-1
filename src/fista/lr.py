68b3823fbb88ae91ec77dea1fccde6ecfb8b3d2b
Ola Idczak <olaa.idczak@gmail.com>
2026-03-17 16:28:44 +0100

    add methods plot and plot_coefficients

--- src/fista/lr.py
+++ src/fista/lr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from measures import *
 
 MEASURES = {
@@ -18,8 +19,16 @@ class LogisticRegression:
         self.X = None
         self.y = None
         self.lmbd = None
-        self.betas = None
-        self.results = None
+        self.betas = {}
+        results = {
+            "recall": [],
+            "precision": [],
+            "f1": [],
+            "bal_acc": [],
+            "roc_auc": [],
+            "pr_auc": [],
+        }
+        self.results = results
 
     def sigmoid(self, X: pd.DataFrame):
         return 1 / (1 + np.exp(-X))
@@ -68,29 +77,55 @@ class LogisticRegression:
             raise ValueError(f"Unsupported measure: {measure}")
 
         lambdas = [0.01, 0.05, 0.1, 0.3, 0.5, 0.7, 1, 2, 3, 5, 10]
-        results = []
-        betas = {}
 
         for l in lambdas:
             self.fit(self.X, self.y, lmbd=l)
             y_score = self.predict_proba(X_valid)
             y_pred = y_score > 0.5
             val = MEASURES[measure].evaluate(y_valid, y_pred, y_score)
-            results.append(val)
-            betas[str(l)] = self.beta
+            self.results[measure].append(val)
+            self.betas[l] = self.beta
 
-        best_id = np.argmax(results)
+        best_id = np.argmax(self.results[measure])
         self.lmbd = lambdas[best_id]
-        self.beta = betas[str(self.lmbd)]
-        self.betas = betas
-        self.results = results
+        self.beta = self.betas[self.lmbd]
 
     def predict_proba(self, X_test: pd.Dataframe):
         XB = X_test @ self.beta
         return self.sigmoid(XB)
 
     def plot(self, measure):
-        pass
+        if measure not in MEASURES:
+            raise ValueError(f"Unsupported measure: {measure}")
+
+        if not self.results[measure]:
+            raise ValueError(
+                f"No results for {measure}. Perform method validate(X_valid, y_valid, {measure}) first."
+            )
+
+        x = sorted(self.betas.keys(), key=float)
+        y = self.results[measure]
+        plt.figure()
+        plt.plot(x, y, marker="o")
+        plt.xlabel("Lambda")
+        plt.ylabel(measure)
+        plt.title(f"{measure} vs lambda ")
+        plt.show()
 
     def plot_coefficients(self):
-        pass
+        lambdas = np.array([float(k) for k in self.betas.keys()])
+        sorted_idx = np.argsort(lambdas)
+        lambdas = lambdas[sorted_idx]
+        coeffs = np.array([self.betas[l] for l in lambdas])
+        n_coeffs = coeffs.shape[1]
+        
+        fig, ax = plt.subplots(figsize=(10, 7))
+        for i in range(n_coeffs):
+            plt.plot(lambdas, coeffs[:, i], label=f"b{i}")
+
+        ax.set_xlabel("Lambda")
+        ax.set_ylabel("Coefficient value")
+        ax.set_title("Coefficient value depending on regularizarion strength")
+        ax.legend()
+        # plt.xscale("log")  
+        # ax.show()
